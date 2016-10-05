e1a41900bb97afcf758858cd82d19daef8c04478
Victor Avdeev <victoravdeev.ac@gmail.com>
2016-10-05 01:40:46 +0000

    Week_5_warmup

---
+++ task_01/peanut.py
@@ -0,0 +1,6 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+"""Peanut module"""
+
+BUTTER = True
+OIL = False
---
+++ task_03.py
@@ -0,0 +1,7 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+"""Copying module attributes"""
+
+import copy
+from task_01.peanut import BUTTER
+JELLY = copy.copy(BUTTER)
